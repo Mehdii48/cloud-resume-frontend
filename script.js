dbf8d66dcdc5f7ccc10c498ceda20e03109301c6
Mehdii48 <2024_mehdi.bedhri@iticbcn.cat>
2026-01-11 14:55:14 +0100

    v4

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// Muestra algo mientras llega la respuesta
 document.getElementById("counter").innerText = "loading...";
+
+// Llamada a tu API Gateway
+fetch("https://56spe8rw4a.execute-api.us-east-1.amazonaws.com/prod/count")
+  .then(response => response.json())
+  .then(data => {
+    // Aquí actualizamos el contador con el valor real
+    document.getElementById("counter").innerText = data.count;
+  })
+  .catch(err => {
+    console.error(err);
+    document.getElementById("counter").innerText = "error";
+  });
